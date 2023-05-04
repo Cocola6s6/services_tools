19b621cf0fe61abd92b826e644c5b090ca9f84a4
Cocola6s6 <799497008@qq.com>
2023-05-04 14:02:20 +0800

    delete docker-hub

--- docker-hub/reRun.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-SERVICE_NAME=$1
-docker stop $SERVICE_NAME
-docker rm $SERVICE_NAME
-sh run.sh
-docker logs -f $SERVICE_NAME
--- docker-hub/run.sh
+++
@@ -1,1 +0,0 @@
-docker run -itd -v /home/docker-hub/data/registry:/var/lib/registry -p 5000:5000 -p 5001:8080 --restart=always --name registry registry:latest
