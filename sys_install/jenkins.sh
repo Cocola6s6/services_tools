6691b725a4323652a25735a8a2101e334d2dd9b2
Cocola6s6 <799497008@qq.com>
2023-05-23 19:51:46 +0800

    update

--- sys_install/jenkins.sh
+++ sys_install/jenkins.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -e
 
-rm -f /home/docker_library/server/*.jar
-\cp /home/services_data/jenkins/jenkins_home/workspace/Gpt_First/Gpt_First/target/Gpt_First-0.0.1-SNAPSHOT.jar /home/docker_library/server
 cd /home/docker_library/server
+rm -f *.jar
+\cp /home/services_data/jenkins/jenkins_home/workspace/Gpt_First/Gpt_First/target/Gpt_First-0.0.1-SNAPSHOT.jar /home/docker_library/server
 sh build.sh
 cd /home/sys_install
 sh install.sh ri server
